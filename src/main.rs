219dbab293be937526e9264eaffac66e67a108fa
JinyuanZhou <jinyuanleo@gmail.com>
2026-06-30 16:16:07 +0800

    fix: report unknown commands

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+    
     print!("$ ");
+    io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    print!("{command}: command not found");
     io::stdout().flush().unwrap();
 }
