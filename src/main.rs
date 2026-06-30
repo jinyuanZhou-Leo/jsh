9102b27db6469e6888401119141572d2764abb05
JinyuanZhou <jinyuanleo@gmail.com>
2026-06-30 16:25:21 +0800

    feat: keep the shell running in a loop

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    print!("{}: command not found\n", command.trim());
-    io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).unwrap();
+
+        print!("{}: command not found\n", command.trim());
+        io::stdout().flush().unwrap();
+    }
 }
