1854bcce28924973209343de43ed92de71975d02
JinyuanZhou <jinyuanleo@gmail.com>
2026-07-02 10:36:33 +0800

    fix: skip the command name when passing argv to external commands

--- src/shell.rs
+++ src/shell.rs
@@ -94,7 +94,7 @@ impl Shell {
             code
         } else {
             if let Some(dir) = self.command_loader().find_executable(&cmd) {
-                match Command::new(dir).args(&argv[..]).status(){
+                match Command::new(dir).args(&argv[1..]).status(){
                     Ok(exit_status) =>{
                         exit_status.code().unwrap()
                     },
